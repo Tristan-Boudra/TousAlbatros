81cff467663853e9b334b3cd6e7bc47f13ed0a75
Tristan-Boudra <boudratristan@gmail.com>
2023-07-22 15:47:05 +0200

    feat(navbar): design of navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#36BDE8',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
